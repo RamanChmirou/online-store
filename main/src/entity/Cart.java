0474b8dcafa1fb968e11155b739da3f90391c4c7
Roman Chmirov <chmirov2006@gmail.com>
2026-04-04 00:15:01 +0200

    +task12

--- main/src/entity/Cart.java
+++ main/src/entity/Cart.java
@@ -1,16 +1,29 @@
 package entity;
 
+import entity.discount.Discount;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
     private final List<CartItem> cartItemList = new ArrayList<>();
+    private Discount discount = null;
+
+    public Cart(){}
+    
+    public Cart(Discount discount) {
+        this.discount = discount;
+    }
 
     public BigDecimal calculateTotalPrice() {
-        return cartItemList.stream()
+        BigDecimal totalPrice = cartItemList.stream()
                 .map(cartItem -> cartItem.getPrice().multiply(BigDecimal.valueOf(cartItem.getQuantity())))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+        if (discount != null) {
+            totalPrice = discount.applyDiscount(totalPrice);
+        }
+        return totalPrice;
     }
 
     public void addCartItem(CartItem cartItem) {
